7a5ee4a7022ef80aabbb136785c85670ac6590ae
Eslam Mohammed <42094349+mreslamgeek@users.noreply.github.com>
2020-12-19 20:30:34 +0200

    post request

--- app/src/main/java/com/eslam/restapi_facebook_simpleapp/ApiInterface.java
+++ app/src/main/java/com/eslam/restapi_facebook_simpleapp/ApiInterface.java
@@ -1,9 +1,24 @@
 package com.eslam.restapi_facebook_simpleapp;
 
+import java.util.HashMap;
+import java.util.List;
+
 import retrofit2.Call;
+import retrofit2.http.Body;
 import retrofit2.http.GET;
+import retrofit2.http.POST;
+import retrofit2.http.Query;
 
 interface ApiInterface {
-    @GET("posts/2")
-    public Call<Post> getPost();
+    @GET("posts")
+    Call<List<Post>> getPost(@Query("userId") String userId);
+
+    // public Call<Post> getPost();
+
+    @POST("posts")
+    //to sent with hash map for multiple vars
+    Call<Post> storePost(@Body HashMap<Object, Object> map);
+
+    //to sent post with class we already have
+    //public Call<Post> storePost(@Body Post post);
 }
--- app/src/main/java/com/eslam/restapi_facebook_simpleapp/MainActivity.java
+++ app/src/main/java/com/eslam/restapi_facebook_simpleapp/MainActivity.java
@@ -5,6 +5,8 @@ import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import java.util.HashMap;
+
 import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
@@ -21,6 +23,12 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         TextView post_title = findViewById(R.id.post_title_txt);
 
+        //Post post = new Post(10, "My Post Request", "eslam mohamed post request test");
+        HashMap<Object , Object> map =new HashMap<>();
+        map.put("title" , "Israa");
+        map.put("body" , "hi, this is my second post request");
+        map.put("userId" , 1001);
+
         Retrofit retrofit = new Retrofit.Builder()
                 .baseUrl(BASE_URL)
                 .addConverterFactory(GsonConverterFactory.create())
@@ -28,9 +36,26 @@ public class MainActivity extends AppCompatActivity {
 
         ApiInterface apiInterface = retrofit.create(ApiInterface.class);
 
-        Call<Post> call = apiInterface.getPost();
+        //Get request
+       /*
+        Call<List<Post>> call = apiInterface.getPost("1");
+        //Call<Post> call = apiInterface.getPost();
 
-        call.enqueue(new Callback<Post>() {
+        call.enqueue(new Callback<List<Post>>() {
+            @Override
+            public void onResponse(Call<List<Post>> call, Response<List<Post>> response) {
+                post_title.setText(response.body().get(0).getTitle());
+            }
+
+            @Override
+            public void onFailure(Call<List<Post>> call, Throwable t) {
+                post_title.setText(t.getMessage());
+
+            }
+        });
+*/
+
+        /*call.enqueue(new Callback<Post>() {
             @Override
             public void onResponse(Call<Post> call, Response<Post> response) {
                 post_title.setText(response.body().getTitle());
@@ -41,6 +66,21 @@ public class MainActivity extends AppCompatActivity {
             public void onFailure(Call<Post> call, Throwable t) {
                 post_title.setText(t.getMessage());
 
+            }
+        });*/
+
+        //Post Request
+       // Call<Post> call = apiInterface.storePost(post);
+        Call<Post> call = apiInterface.storePost(map);
+        call.enqueue(new Callback<Post>() {
+            @Override
+            public void onResponse(Call<Post> call, Response<Post> response) {
+                post_title.setText(response.body().getTitle());
+            }
+
+            @Override
+            public void onFailure(Call<Post> call, Throwable t) {
+                post_title.setText(t.getMessage());
             }
         });
 
